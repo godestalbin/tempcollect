18db5a6e9be62bd69ba85227a9f0c50ed197c77b
godestalbin <godestalbin@gmail.com>
2020-12-22 21:16:17 +0100

    Removed import git

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request
 from flaskapp import tempCollectApp
 from flaskapp import firbeix
 from flaskapp import wattignies
-import git
+# import git
 
 @tempCollectApp.route('/')
 @tempCollectApp.route('/index')
