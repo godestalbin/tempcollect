1083f2c926f9dde8d9e1273e4b6eac67ee545d71
godestalbin <godestalbin@gmail.com>
2020-12-25 15:03:12 +0100

    Added Highcharts

--- classes/mongodb.py
+++ classes/mongodb.py
@@ -16,9 +16,11 @@ class MongoDb:
         self.collection.insert_one(record)  
 
     def get(self, filter):
-        return self.collection.find_one(filter)
+        return self.collection.find(filter)
 
 # Used for testing
 # mongo = MongoDb()
 # # mongo.add({'city': 'Firbeix', 'temp': 8.1})
 # print(mongo.get({'city': 'Firbeix'}))
+# for temp in mongo.get({'city': 'Firbeix'}):
+#     print(temp['date'], temp['temp'])
--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,11 +1,15 @@
+import platform
 from flask import Flask
 tempCollectApp = Flask(__name__)
 
 from classes.openweather import OpenWeather
 from classes.datacollect import DataCollect
+from classes.mongodb import MongoDb
 
 firbeix = OpenWeather("Firbeix")
 wattignies = OpenWeather("Wattignies")
-dataCollect = DataCollect() 
+mongo = MongoDb()
+
+if platform.system() == 'Linux': dataCollect = DataCollect()
 
 from flaskapp import routes
